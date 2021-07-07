df93026d6dd2c6b33b40e6914ef89317d2689d27
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-07-07 18:47:25 +0200

    b

--- src/model.py
+++ src/model.py
@@ -41,6 +41,7 @@ class TransformerClassifier(pl.LightningModule):
         print('train start', self.device)
 
     def forward(self, x):
+        print('forward', self.device)
         self.transformer = self.transformer.to(self.device)
         features = self.transformer.tokenize(x)
         embeddings = self.transformer(features)['sentence_embedding']
