61636cc0f5123792bc23b7f0bae5ffc527c6dcfa
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-07-07 18:45:29 +0200

    test

--- src/model.py
+++ src/model.py
@@ -37,10 +37,11 @@ class TransformerClassifier(pl.LightningModule):
         self.val_confusion = StatScores(num_classes=num_classes)
 
     def setup(self, stage):
-        self.transformer.to(self.device)
+        self.transformer = self.transformer.to(self.device)
         print('train start', self.device)
 
     def forward(self, x):
+        self.transformer = self.transformer.to(self.device)
         features = self.transformer.tokenize(x)
         embeddings = self.transformer(features)['sentence_embedding']
         # print(embeddings['sentence_embedding'], embeddings['sentence_embedding'].shape, embeddings['cls_token_embeddings'], embeddings['cls_token_embeddings'].shape)
