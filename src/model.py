b52bda7ddebf9f428b637de4c04c5fc2c699cbb0
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-07-07 21:10:10 +0200

    f1loss

--- src/model.py
+++ src/model.py
@@ -8,8 +8,60 @@ import pytorch_lightning as pl
 from torch import optim, nn, sigmoid
 from torchmetrics import Accuracy, F1
 from sentence_transformers import SentenceTransformer, models
+import torch
+class F1Loss(nn.Module):
+    '''Calculate F1 score. Can work with gpu tensors
 
+    The original implmentation is written by Michal Haltuf on Kaggle.
 
+    Returns
+    -------
+    torch.Tensor
+        `ndim` == 1. epsilon <= val <= 1
+
+    Reference
+    ---------
+    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
+    #sklearn.metrics.f1_score
+    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html
+    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
+    - http://www.ryanzhang.info/python/writing-your-own-loss-function-module-for-pytorch/
+    '''
+
+    def __init__(self, epsilon=1e-10, reduction='mean', pos_weight=None):
+        super().__init__()
+        self.epsilon = epsilon
+        self.reduction = reduction
+
+    def forward(self, predicted, actual):
+        if predicted.ndim != 1 or actual.ndim != 1:
+            print(
+                f"Loss calculation, encountered following input shapes: predicted: {predicted.shape} {predicted}, actual: {actual.shape} {actual}")
+        assert predicted.ndim <= 1
+        assert actual.ndim <= 1
+
+        predicted = torch.sigmoid(predicted)
+
+        tp = (actual * predicted).sum().to(torch.float32)
+        # tn = ((1 - actual) * (1 - predicted)).sum().to(torch.float32)
+        fp = ((1 - actual) * predicted).sum().to(torch.float32)
+        fn = (actual * (1 - predicted)).sum().to(torch.float32)
+
+        precision = tp / (tp + fp + self.epsilon)
+        recall = tp / (tp + fn + self.epsilon)
+
+        f1 = 2 * (precision * recall) / (precision + recall + self.epsilon)
+        # f1 = f1.clamp(min=self.epsilon, max=1-self.epsilon)
+
+        # return 1 - f1 to minimize
+        if self.reduction == 'mean':
+            # f1 is already mean
+            return 1 - f1
+        elif self.reduction == "sum":
+            # simulate seperate loss for each observation in input (:= "sum")
+            return (1 - f1) * len(predicted)
+        else:
+            return 1 - f1
 class TransformerClassifier(pl.LightningModule):
     def __init__(self, lr=0.001, num_classes=1) -> None:
         super().__init__()
@@ -27,7 +79,8 @@ class TransformerClassifier(pl.LightningModule):
             nn.ReLU(),
             nn.Linear(334, 1)
         )
-        self.loss = nn.BCEWithLogitsLoss()
+        # self.loss = nn.BCEWithLogitsLoss()
+        self.loss = F1Loss()
 
         shared_metrics = kdict(accuracy=Accuracy(num_classes=num_classes),
                                f1=F1(num_classes=num_classes))
