431d9e0dad83bae6165be7e7a3105c0a7d47e2b8
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-07-08 04:22:21 +0200

    sd

--- src/model.py
+++ src/model.py
@@ -82,14 +82,14 @@ class TransformerClassifier(pl.LightningModule):
         self.loss = nn.BCEWithLogitsLoss()
         # self.loss = F1Loss()
 
-        shared_metrics = kdict(accuracy=Accuracy(num_classes=num_classes, dist_sync_on_step=True),
-                               f1=F1(num_classes=num_classes, dist_sync_on_step=True))
+        shared_metrics = kdict(accuracy=Accuracy(num_classes=num_classes),
+                               f1=F1(num_classes=num_classes))
         self.metrics = kdict(
             train=shared_metrics.copy(),
             val=shared_metrics.copy(),
             test=shared_metrics.copy())
 
-        self.val_confusion = StatScores(num_classes=num_classes, dist_sync_on_step=True)
+        self.val_confusion = StatScores(num_classes=num_classes)
         self.test = []
 
     # def setup(self, stage):
