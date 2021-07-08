689fc8b083bacd579c6eacd35798ba8f637976c9
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-07-08 14:59:49 +0200

    rm rank

--- src/model.py
+++ src/model.py
@@ -74,9 +74,8 @@ class TransformerClassifier(pl.LightningModule):
         return self._step("val", batch)
 
     def validation_epoch_end(self, outputs) -> None:
-        if _get_rank() == 0:
-            print(self.val_confusion.compute())
-            self.val_confusion.reset()
+        print(self.val_confusion.compute())
+        self.val_confusion.reset()
 
         print(len([val for val in self.class_balance_check if val == 0]),
               len([val for val in self.class_balance_check if val == 1]))
